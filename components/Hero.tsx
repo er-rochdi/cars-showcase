bafaf15cab111f86701cb6df8e158e0733d2265f
KARA <kara@KARAs-MacBook-Pro.local>
2024-12-12 11:52:06 +0100

    add component custombutton

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,25 @@
-
+"use client";
+import { CustomButton } from '@/components'
 const Hero = () => {
+    const handleScroll = () => {
+
+    };
     return (
-        <div>
-            Hero
+        <div className="hero">
+            <div className=" flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, book, rent a car—quick and super easy!
+                </h1>
+                <p className="hero__subtitle">
+                    Streamline your car rental experience with our effortless booking
+                    process.
+                </p>
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
         </div>
     )
 }
